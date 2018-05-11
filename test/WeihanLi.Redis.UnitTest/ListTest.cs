87b68b51baa4b1bcff6c0387f2c2d1ab77e62583
liweihan <weihanli@outlook.com>
2018-05-11 14:02:43 +0800

    update unit test

--- test/WeihanLi.Redis.UnitTest/ListTest.cs
+++ test/WeihanLi.Redis.UnitTest/ListTest.cs
@@ -8,16 +8,21 @@ namespace WeihanLi.Redis.UnitTest
         public void MainTest()
         {
             var testListKey = "testList";
+            var commonClient = RedisManager.GetCommonRedisClient(RedisDataType.List);
+            Assert.False(commonClient.KeyExists(testListKey));
             var listClient = RedisManager.GetListClient<string>(testListKey);
+            Assert.False(commonClient.KeyExists(testListKey));
             Assert.Equal(0, listClient.Count());
             listClient.Push(new[] { "Michael", "Kangkang", "Kangkang" });
+            Assert.True(commonClient.KeyExists(testListKey));
             Assert.Equal(3, listClient.Count());
             Assert.Equal("Kangkang", listClient.Pop());
             Assert.Equal(2, listClient.Count());
             Assert.Equal("Michael", listClient.LeftPop());
             Assert.Equal(1, listClient.Count());
-            var commonClient = RedisManager.GetCommonRedisClient(RedisDataType.List);
+
             Assert.True(commonClient.KeyDelete(testListKey));
+            Assert.False(commonClient.KeyExists(testListKey));
         }
     }
 }
--- test/WeihanLi.Redis.UnitTest/StringTest.cs
+++ test/WeihanLi.Redis.UnitTest/StringTest.cs
@@ -12,6 +12,8 @@ namespace WeihanLi.Redis.UnitTest
             var key = "test111";
             var value = "Hello WeihanLi.Redis";
             var cacheClient = RedisManager.CacheClient;
+            Assert.False(cacheClient.Exists(key));
+            Assert.Null(cacheClient.Get(key));
             Assert.True(cacheClient.Set(key, value));
             Assert.True(cacheClient.Exists(key));
             Assert.Equal(value, cacheClient.Get(key));

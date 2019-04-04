62abbd0e219f5772c996c280445cee1f099c96fb
WeihanLi <weihanli@outlook.com>
2019-04-04 17:45:17 +0800

    update StackExchange.Redis to 2.0.588

--- build/applyVersionSuffix.sh
+++ build/applyVersionSuffix.sh
@@ -1,4 +1,8 @@
 VersionSuffix="preview-$(date -u +"%Y%m%d-%H%M%S")"
+if ["$(Build.SourceBranchName)" == "master"]
+then
+    VersionSuffix=""
+fi
 echo "VersionSuffix:$VersionSuffix"
 sed -i "s/<VersionSuffix.*>.*<\/VersionSuffix>/<VersionSuffix>$VersionSuffix<\/VersionSuffix>/" $BUILD_SOURCESDIRECTORY/build/version.props
 cat $BUILD_SOURCESDIRECTORY/build/version.props

4690a8a8ab91b8958a03d6550cb84ab0156cb012
WeihanLi <weihanli@outlook.com>
2019-04-30 12:16:17 +0800

    add ConfigurationChangedEventSample

--- samples/WeihanLi.Redis.Samples/Program.cs
+++ samples/WeihanLi.Redis.Samples/Program.cs
@@ -3,8 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using WeihanLi.Common;
 using WeihanLi.Common.Helpers;
-using WeihanLi.Common.Models;
-using WeihanLi.Extensions;
+using WeihanLi.Common.Logging.Log4Net;
 
 namespace WeihanLi.Redis.Samples
 {
@@ -12,24 +11,37 @@ namespace WeihanLi.Redis.Samples
     {
         public static void Main(string[] args)
         {
+            LogHelper.AddLogProvider(new Log4NetLogHelperProvider());
+
             IServiceCollection services = new ServiceCollection();
-            services.AddRedisConfig(options => { options.EnableCompress = true; });
+            services.AddRedisConfig(options =>
+            {
+                options.EnableCompress = true;
+                options.RedisServers = new[]
+                {
+                    new RedisServerConfiguration("127.0.0.1"),
+                    new RedisServerConfiguration("127.0.0.1", 16379),
+                    new RedisServerConfiguration("127.0.0.1", 26379),
+                };
+            });
             // custom serializer
             services.AddSingleton<IDataSerializer, BinaryDataSerializer>();
             services.AddSingleton<IDataCompressor, MockDataCompressor>();
             DependencyResolver.SetDependencyResolver(services);
 
-            var cacheClient = DependencyResolver.Current.ResolveService<ICacheClient>();
-            var customSerializerCacheKey = "TestCustomSerializer";
-            cacheClient.Set(customSerializerCacheKey, new PagedListModel<int>()
-            {
-                Data = new int[0],
-                PageNumber = 2,
-                PageSize = 10,
-                TotalCount = 10,
-            }, TimeSpan.FromMinutes(1));
-            var result = cacheClient.Get<PagedListModel<int>>(customSerializerCacheKey);
-            Console.WriteLine(result.ToJson());
+            //var cacheClient = DependencyResolver.Current.ResolveService<ICacheClient>();
+            //var customSerializerCacheKey = "TestCustomSerializer";
+            //cacheClient.Set(customSerializerCacheKey, new PagedListModel<int>()
+            //{
+            //    Data = new int[0],
+            //    PageNumber = 2,
+            //    PageSize = 10,
+            //    TotalCount = 10,
+            //}, TimeSpan.FromMinutes(1));
+            //var result = cacheClient.Get<PagedListModel<int>>(customSerializerCacheKey);
+            //Console.WriteLine(result.ToJson());
+
+            ConfigurationChangedEventSample.MainTest();
 
             Console.ReadLine();
         }
